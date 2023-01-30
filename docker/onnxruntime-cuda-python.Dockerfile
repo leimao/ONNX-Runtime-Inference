e935b87608575b6308883d8d168361f40c94dc61
Lei Mao <dukeleimao@gmail.com>
2023-01-29 19:58:43 -0800

    Add Python Example

---
+++ docker/onnxruntime-cuda-python.Dockerfile
@@ -0,0 +1,56 @@
+FROM nvcr.io/nvidia/cuda:11.6.2-cudnn8-devel-ubuntu20.04
+
+ARG ONNXRUNTIME_VERSION=1.12.0
+
+ENV DEBIAN_FRONTEND noninteractive
+
+# Install package dependencies
+RUN apt-get update && apt-get install -y --no-install-recommends \
+        build-essential \
+        software-properties-common \
+        autoconf \
+        automake \
+        libtool \
+        pkg-config \
+        ca-certificates \
+        wget \
+        git \
+        curl \
+        libjpeg-dev \
+        libpng-dev \
+        language-pack-en \
+        locales \
+        locales-all \
+        python3 \
+        python3-py \
+        python3-dev \
+        python3-pip \
+        python3-numpy \
+        python3-pytest \
+        python3-setuptools \
+        libprotobuf-dev \
+        protobuf-compiler \
+        zlib1g-dev \
+        swig \
+        vim \
+        gdb \
+        valgrind \
+        libsm6 \
+        libxext6 \
+        libxrender-dev \
+        cmake \
+        unzip
+RUN apt-get clean
+
+RUN cd /usr/local/bin && \
+    ln -s /usr/bin/python3 python && \
+    ln -s /usr/bin/pip3 pip && \
+    pip install --upgrade pip setuptools wheel
+
+# System locale
+# Important for UTF-8
+ENV LC_ALL en_US.UTF-8
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US.UTF-8
+
+RUN pip install Pillow==9.4.0 onnx==${ONNXRUNTIME_VERSION} onnxruntime-gpu==${ONNXRUNTIME_VERSION}
---
+++ python/inference.py
@@ -0,0 +1,98 @@
+import numpy as np
+import onnx
+from onnx import numpy_helper
+import onnxruntime as ort
+from onnx import TensorProto
+from PIL import Image
+from typing import List
+
+
+def preprocess(image_data: np.ndarray) -> np.ndarray:
+
+    mean_vec = np.array([0.485, 0.456, 0.406])
+    stddev_vec = np.array([0.229, 0.224, 0.225])
+    norm_image_data = np.zeros(image_data.shape).astype('float32')
+    for i in range(image_data.shape[0]):
+        # For each pixel in each channel,
+        # divide the value by 255 to get value between [0, 1] and then normalize.
+        norm_image_data[i, :, :] = (image_data[i, :, :] / 255 -
+                                    mean_vec[i]) / stddev_vec[i]
+    return norm_image_data
+
+
+def read_label_data(label_file_path: str) -> List[str]:
+
+    labels = []
+    with open(label_file_path, "r") as fhand:
+        for line in fhand:
+            labels.append(line.strip())
+
+    return labels
+
+
+def read_test_data(tensor_proto_file_path: str) -> np.ndarray:
+
+    # Option 1: Using TensorProto APIs.
+    # tensor = TensorProto()
+    # with open(tensor_proto_file_path, "rb") as proto_file:
+    #     tensor.ParseFromString(proto_file.read())
+    # Option 2: Using ONNX wrapper function instead.
+    tensor = onnx.load_tensor(tensor_proto_file_path)
+    numpy_array = numpy_helper.to_array(tensor)
+
+    return numpy_array
+
+
+if __name__ == "__main__":
+
+    onnx_model_file_path = "../data/models/resnet18-v1-7.onnx"
+    test_input_file_path = "../data/test_data/resnet18-v1-7/test_data_set_0/input_0.pb"
+    test_output_file_path = "../data/test_data/resnet18-v1-7/test_data_set_0/output_0.pb"
+    image_file_path = "../data/images/european-bee-eater-2115564_1920.jpg"
+    label_file_path = "../data/labels/synset.txt"
+
+    model_proto = onnx.load(onnx_model_file_path)
+    onnx.checker.check_model(model_proto)
+    model_proto_bytes = onnx._serialize(model_proto)
+    # Create ONNX Runtime inference session.
+    # https://onnxruntime.ai/docs/get-started/with-python.html
+    ort_sess = ort.InferenceSession(
+        model_proto_bytes,
+        providers=["CPUExecutionProvider", "CUDAExecutionProvider"])
+
+    # Read IO tensor information.
+    # https://onnxruntime.ai/docs/api/python/api_summary.html#onnxruntime.NodeArg
+    input_nodes = ort_sess.get_inputs()
+    input_names = [node.name for node in input_nodes]
+    input_shapes = [node.shape for node in input_nodes]
+    input_types = [node.type for node in input_nodes]
+    output_nodes = ort_sess.get_outputs()
+    output_names = [node.name for node in output_nodes]
+    output_shapes = [node.shape for node in output_nodes]
+    output_types = [node.type for node in output_nodes]
+
+    # Read test input and output files.
+    input_array = read_test_data(tensor_proto_file_path=test_input_file_path)
+    output_array = read_test_data(tensor_proto_file_path=test_output_file_path)
+
+    # Run unit test.
+    output_tensors = ort_sess.run(output_names=output_names,
+                                  input_feed={input_names[0]: input_array},
+                                  run_options=None)
+    assert np.allclose(output_tensors[0], output_array)
+
+    # Read labels.
+    labels = read_label_data(label_file_path=label_file_path)
+
+    image_data = np.asarray(Image.open(image_file_path).resize(
+        (224, 224))).transpose(2, 0, 1)
+    normalized_image_data = preprocess(image_data=image_data)
+    normalized_image_data = np.expand_dims(normalized_image_data, axis=0)
+    output_tensors = ort_sess.run(
+        output_names=output_names,
+        input_feed={input_names[0]: normalized_image_data},
+        run_options=None)
+    predicted_label_idx = np.argmax(output_tensors[0][0])
+
+    print(f"Predicted Label ID: {predicted_label_idx}")
+    print(f"Predicted Label: {labels[predicted_label_idx]}")
